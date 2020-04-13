4c00bcba485335f41b6cebed4f5b3f6ae38bd5b5
Atsolind <atso.lindstrom@student.lut.fi>
2020-04-13 19:51:02 +0300

    Assignment 10.1

---
+++ app/src/main/java/com/example/webapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.webapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    EditText editText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        editText = (EditText) findViewById(R.id.editText);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)){
+                    String url = editText.getText().toString();
+                    webView.loadUrl("http://"+url);
+                }
+                return false;
+            }
+        });
+
+    }
+
+}
